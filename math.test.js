7161c0b901d1d186b92447dc53dce84aff89e9d5
vegard-hd <vegard.hd@outlook.com>
2025-06-01 11:39:20 +0200

    minor change; should fail CI

--- math.test.js
+++ math.test.js
@@ -2,6 +2,6 @@ const { addNumbers } = require('./math');
 
 describe('math.js', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(addNumbers(1, 2)).toBe(3);
+    expect(addNumbers(1, 2)).toBe(4);
   });
 });
